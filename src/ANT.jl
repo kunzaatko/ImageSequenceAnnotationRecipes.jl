ef296fced4c3e39503dcee441630258edec4ab68
Martin Kunz <martinkunz@email.cz>
2022-04-18 13:45:26 +0200

    feat(locations): recipe locations on frames
    Made a Makie recipe for locations with a special type which is
    associated with it and added this plot to each frame of the
    annotationtool, making it invisible on frame change.

--- src/ANT.jl
+++ src/ANT.jl
@@ -1,36 +1,70 @@
 module ANT
 using Makie, Images, DataStructures
+include("locations.jl")
+
 export annotationtool, HOTKEYS
-HOTKEYS = Dict{Symbol,Any}(:prev_frame => [Keyboard.left, Keyboard.h], :next_frame => [Keyboard.right, Keyboard.l], :add_loc => [Keyboard.a], :del_loc => [Keyboard.d],
+
+# FIX: This type should be specified strictly so that it can be relied on <06-04-22> 
+# TODO: Annotation hotkeys should include numbers... How to add them? <07-04-22> 
+HOTKEYS = Dict{Symbol,Any}(
+    :prev_frame => [Keyboard.left, Keyboard.h],
+    :next_frame => [Keyboard.right, Keyboard.l],
+    :add_loc => [Keyboard.a],
+    :del_loc => [Keyboard.d],
+    :select_loc => [Keyboard.s],
     :annotation => [[Keyboard.q], [Keyboard.w], [Keyboard.e], [Keyboard.r], [Keyboard.t]])
 
-function annotationtool(im::AbstractArray{<:Colorant,3}; hotkeys = HOTKEYS, colors = COLORS, 
-    categories = [Symbol("1"), Symbol("2"), Symbol("3"), Symbol("4"), Symbol("5"), Symbol("6")])
+# TODO: Add posibility to drag image into figure with Union{..., Nothing} on im and Event
+# dropped_files <04-04-22> 
+# TODO: previous input insertion <04-04-22> 
+function annotationtool(
+    im::AbstractArray{<:Colorant,3};
+    hotkeys = HOTKEYS,
+    categories = Symbol.(["1", "2", "3", "4", "5", "6"]), # NOTE: Will be determined if the data is passed in
+    locations_attrs = (;)
+)
+
+    #  Scene setup {{{
     fig = Figure()
 
     # AXES
     mainimage_ax = Makie.Axis(fig[1, 1], aspect = size(im)[1] / size(im)[2], limits = ((0, size(im)[1]), (0, size(im)[2])))
     frameslider_pos = fig[2, 1]
 
+    # TODO: Add minimap to left top corner of mainimage_ax, that will show all the things as in the
+    # true image but smaller.. including the locations <06-04-22> 
+
     # State Observables
     frameslider = Slider(frameslider_pos, range = 1:size(im)[3])
     curframe = frameslider.value
-    Label(frameslider_pos, lift(idx -> "$(idx)/$(size(im)[3])", curframe), tellwidth = false )
+    Label(frameslider_pos, lift(idx -> "$(idx)/$(size(im)[3])", curframe), tellwidth = false)
+    # }}}
 
-    frameplots = Vector{Observable{Dict{Symbol,AbstractPlot}}}(undef, size(im)[3])
+    # A collection of plots associated with the frame -> when the frame changes, previous frame
+    # associated plots are made invisible and the plots for the current frame are shown
+    frameplots = Vector{Dict{Symbol,AbstractPlot}}(undef, size(im)[3])
     for i in 1:length(frameplots)
-        frameplots[i] = Observable(Dict{Symbol,AbstractPlot}())
+        frameplots[i] = Dict{Symbol,AbstractPlot}()
     end
 
-    curplots = @lift frameplots[$curframe][]
+    # TODO: Should be done with PriorityObservable
+    curplots = @lift frameplots[$curframe]
+    on(curplots) do plots
+        # TODO: Should be split into more Priority Ons that
+        # 1. One that creates the plot if it does not exist -> Bigger priority
+        # 2. One that shows the plots that are neccesarry to show 
+        # 3. One that restores the limits <kunzaatko> 
+        for (_, plot) in plots
+            # @debug "Making $plot visible on frame $(curframe[])"
+            plot.visible = true
+        end
+    end
+
     on(curplots) do plots
         if !haskey(plots, :image) # image for this frame is not yet created
             lims = mainimage_ax.finallimits[] # limits to be restored on frame change
             plots[:image] = image!(mainimage_ax, im[:, :, curframe[]], interpolate = false, inspectable = false)
             limits!(mainimage_ax, lims)
-        end
-        for (_, plot) in plots
-            plot.visible = true
         end
     end
 
@@ -41,23 +75,19 @@ function annotationtool(im::AbstractArray{<:Colorant,3}; hotkeys = HOTKEYS, colo
 
     # On change of frame
     on(curframe) do frame
-        # {{{
-        # NOTE: Making the previous plots invisible <04-04-22> 
-        foreach(f -> begin
-                if f != frame
-                    for (_, plot) in frameplots[f][]
+        foreach(f -> begin # make the previous plots invisible
+                if f != frame # NOTE: Checked here, because it is possible to land on the same frame and notifying `curframe` using the slider
+                    @show frameplots[f]
+                    for (_, plot) in frameplots[f]
                         plot.visible = false
                     end
                 end
             end, visitedframes)
         push!(visitedframes, frame)
-        # }}}
     end
-    notify(curframe)
 
     # Global hotkey events
     on(events(fig).keyboardbutton) do event
-        # {{{
         if event.action in (Keyboard.press, Keyboard.repeat)
             for key in hotkeys[:prev_frame]
                 event.key == key && set_close_to!(frameslider, curframe[] - 1)
@@ -67,9 +97,75 @@ function annotationtool(im::AbstractArray{<:Colorant,3}; hotkeys = HOTKEYS, colo
             end
         end
         return Consume(false)
-        # }}}
     end
+
+
+    # Collected data
+    selected_loc = Vector{Node{Selected}}(undef, size(im)[3])
+    for i in 1:length(selected_loc)
+        selected_loc[i] = Node(Selected(nothing))
+    end
+
+    locs = Vector{Vector{Location}}(Vector(undef, size(im)[3]))
+    for i in 1:length(locs)
+        locs[i] = Vector{Location}(undef, 0)
+    end
+    locs = locs |> Node
+
+    olocs = [Node(OffsetVector(locs[], -i)) for i in 1:size(im)[3]]
+
+    on(curplots) do plots
+        if !haskey(plots, :locations) # image for this frame is not yet created
+            lims = mainimage_ax.finallimits[] # limits to be restored on frame change
+            plots[:locations] = locations!(mainimage_ax, selected_loc[curframe[]], olocs[curframe[]]; locations_attrs...)
+            limits!(mainimage_ax, lims)
+        else
+            notify(olocs[curframe[]])
+        end
+    end
+    notify(curframe)
+
+    # TODO: How to represent tracks? <06-04-22> 
+    # tracks = Observable{Vector{Vector}}()
+
+    function add_loc(location)
+        push!(locs[][curframe[]], location)
+        selected_loc[curframe[]][].idx = length(locs[][curframe[]])
+        notify(olocs[curframe[]])
+    end
+
+    # Adding locations
+    on(events(mainimage_ax.scene).mousebutton, priority = 2) do event
+        if event.button == Mouse.left && event.action == Mouse.press
+            if any(ispressed.(mainimage_ax.scene, hotkeys[:add_loc]))
+                # Add location
+                location = Location(curframe[], mouseposition(mainimage_ax.scene))
+                @debug "Adding location on frame $(curframe[]) with value $location"
+                add_loc(location)
+                return Consume(true)
+            else
+                for (category, annotation_keys) in zip(categories, hotkeys[:annotation])
+                    if any(ispressed.(mainimage_ax.scene, annotation_keys))
+                        # Add location with category
+                        location = Location(curframe[], mouseposition(mainimage_ax.scene), category)
+                        @debug "Adding location on frame $(curframe[]) with value $location"
+                        add_loc(location)
+                        return Consume(true)
+                    end
+                end
+            end
+        end
+        return Consume(false)
+    end
+    # TODO: Position with minimap <04-04-22> 
+    # TODO: Keymaps for classification <04-04-22> 
+    # TODO: TickBox for color change and tracks from prev image <04-04-22> 
+    # TODO: Exporting the final data collected into tables <04-04-22> 
+    # TODO: DataInspector <04-04-22> 
 
     display(fig)
 end
+
+# TODO: Method for image with the filename instead of passing the image <06-04-22> 
+
 end
---
+++ src/locations.jl
@@ -0,0 +1,190 @@
+import Makie: convert_arguments, Scatter
+using OffsetArrays
+using ColorSchemes
+using GeometryBasics
+
+export Location, Selected
+
+mutable struct Location
+    time::Integer
+    point::Point{2,Real}
+    category::Union{Nothing,Symbol}
+end
+
+Location(frame::Integer, p::Point) = Location(frame, p, nothing)
+
+Makie.convert_arguments(P::Type{<:Scatter}, locations::Vector{Location}) = convert_arguments(P, getfield.(locations, :point))
+Makie.convert_arguments(P::Type{<:Scatter}, location::Location) = convert_arguments(P, location.point)
+
+@recipe(Locations) do scene
+    Attributes(
+        # marker colorschemes -> below will be darker and above lighter by default
+        colors = Dict{Union{Integer,Symbol},ColorScheme}(
+            0 => ColorSchemes.tol_bu_rd, # default
+            1 => ColorSchemes.Greens_9,
+            2 => ColorSchemes.Purples_9,
+            3 => ColorSchemes.Oranges_9,
+            4 => ColorSchemes.Blues_9,
+        ), # has to include the 0 key for the default colors of the location
+        depth = 0,  # number of location layers to show below current layer
+        height = 0, # number of location layers to show above current layer
+        above_attributes = (; marker = :circle, markersize = 6),
+        below_attributes = (; marker = :square, markersize = 6),
+        base_attributes = (; marker = :cross, markersize = 9),
+        selected_attributes = (; marker = :circle, markersize = 12),
+        visible = true
+    )
+end
+
+# TODO: Handle the colors in a different way so that the colors are muted in both directions the
+# same way... Depth and height can then be distinguished by shape and size for example <11-04-22, kunzaatko> 
+
+argument_names(::Type{<:Locations}) = (:selected, :locations,)
+
+mutable struct Selected
+    idx::Union{Nothing,<:Integer}
+end
+# FIX: When the depth and height are greater that 0, this throws an error <18-04-22> 
+function Makie.plot!(
+    locs::Locations{<:Tuple{Selected,OffsetVector{<:AbstractVector{Location}}}})
+
+    selected = locs[1]
+    locations = locs[2]
+
+    points = Node(OffsetVector(Node(Location[])[], 0))
+    categories = Node(Symbol[])
+    colors = Node(OffsetVector(Node(Colorant[])[], 0))
+    point_selected = (; points = Node{Vector{Point{2,Real}}}(Point[]), colors = Node{Vector{Colorant}}(Vector(undef, 0)))
+    points_notselected = (; points = Node{Vector{Point{2,Real}}}(Point[]), colors = Node{Vector{Colorant}}(Vector(undef, 0)))
+
+    # PERF: This is not necessary to do for all of the updates on any of the variable changes. 
+    # TODO: Should check for and filter the actions that really need to be made if this is too slow.  
+    function on_updateplot(locations, selected, depth, height)
+        # points
+        start_index = max((-depth), (locations.offsets[1] + 1))
+        end_index = min(height, (length(locations) - locations.offsets[1]))
+        points = OffsetVector(map(l -> Node(l), locations[start_index:end_index]), (-depth - 1)) |> Node
+        @debug "`on_updateplot` set points" points
+
+        # categories
+        categories = map(i -> filter(c -> c !== nothing, getfield.(i, :category)), locations) |> Iterators.flatten |> unique |> sort |> Node
+        @assert all(typeof.(categories[]) .== Symbol) "Category not of type symbol at $(typeof.(categories[]) .!= Symbol) where $(categories[][typeof.(categories[]) .!= Symbol])"
+        @debug "`on_updateplot` set categories" categories
+
+        # a dictionary, where every category has an assigned colorscheme -> that is later used for
+        # geneteration of the layer colors for different categories
+        # NOTE: no category, represented by nothing is represented by `:nothing` key in this dict <10-04-22> 
+        cat_col_dict = lift(locs.colors, categories) do colors, categories
+            # FIX: This will not work if there is not enough colors in the attributes <10-04-22> 
+            idx = 0
+            cat_or_ind = [
+                begin
+                    if haskey(colors, cat)
+                        colors[cat]
+                    else
+                        idx += 1
+                        idx
+                    end
+                end for cat in categories
+            ]
+            col_cat = zip(categories, map(c_or_i -> colors[c_or_i], cat_or_ind)) |> Dict{Symbol,ColorScheme}
+            col_cat[:nothing] = colors[0]
+            return col_cat
+        end
+        @debug "`on_updateplot` set cat_col_dict" cat_col_dict
+
+        colors = lift(points, cat_col_dict) do points, cat_col
+            layer_num = locs.depth[] + locs.height[] + 1
+            layer_intensities = if layer_num > 1
+                range(0.3, 1, length = layer_num)
+            else
+                [0.5]
+            end
+            @debug "Colors in `lift` with " layer_num layer_intensities
+            colors = Vector{Observable{Vector{Colorant}}}(undef, layer_num)
+            for (idx, (layer_intensity, layer)) in enumerate(zip(layer_intensities, points))
+                @assert eltype(layer[]) == Location "eltype of layer is $(eltype(layer))"
+                layer_categories = map(loc -> if loc.category !== nothing
+                        loc.category
+                    else
+                        :nothing
+                    end, layer[])
+                @assert all(typeof.(layer_categories) .== Symbol) "Categories not defined correctly. Some type was not `Symbol`"
+                @assert all(map(cat -> haskey(cat_col, cat), layer_categories)) "Not all categories have defined colors in `cat_col_dict`"
+                colors[idx] = map(lcat -> get(cat_col[lcat], layer_intensity), layer_categories) |> Node
+                @assert eltype(colors[idx][]) <: Colorant "Not a good color $(eltype(colors[idx][]))"
+            end
+            colors = OffsetVector(colors, (-locs.depth[] - 1))
+            @assert colors.offsets == points.offsets "colors offsets $(colors.offsets) are not equal to points offsets $(points.offsets)"
+            @assert all(length(c[]) == length(p[]) for (c, p) in zip(colors, points)) "colors and points do not have equal lengths"
+            @debug "setting color in `lift`" colors
+            return colors
+        end
+
+
+        @debug "Running `on_selected` with selected" selected
+        empty!(point_selected.points[])
+        empty!(point_selected.colors[])
+        info = 0
+        while length(colors[][0][]) != length(points[][0][])
+            sleep(1e-4)
+            if info == 0
+                @debug "Waiting for color update in `on_selected`"
+            end
+            info += 1
+            if info == 400
+                @debug "colors" colors[][0][] " with length" length(colors[][0][])
+                @debug "points" points[][0][] " with length" length(points[][0][])
+                return @error "color not the same length as points" colors[] points[]
+            end
+        end
+        if info > 0
+            @debug "Waited $info ms for color update"
+        end
+        if selected.idx !== nothing
+            empty!(points_notselected.points[])
+            empty!(points_notselected.colors[])
+            push!(point_selected.points[], points[][0][][selected.idx].point)
+            push!(point_selected.colors[], colors[][0][][selected.idx])
+            notselected_indexes = collect(1:length(points[][0][]))
+            popat!(notselected_indexes, selected.idx)
+            append!(points_notselected.points[], getfield.(points[][0][][notselected_indexes], :point))
+            append!(points_notselected.colors[], colors[][0][][notselected_indexes])
+            notify(points_notselected.points)
+            notify(points_notselected.colors)
+        else
+            points_notselected.points[] = getfield.(points[][0][], :point)
+            points_notselected.colors[] = colors[][0][]
+        end
+        notify(point_selected.points)
+        notify(point_selected.colors)
+    end
+    on_updateplot(locations[], selected[], locs.depth[], locs.height[]) # starting locations
+    Makie.Observables.onany(on_updateplot, locations, selected, locs.depth, locs.height)
+
+
+    # FIX: This will not work if the depth or height is changed <10-04-22> 
+    if locs.depth[] > 0
+        for (cs, ls) in zip(colors[][begin:-1], points[][begin:-1])
+            scatter!(locs, ls; color = cs, locs.below_attributes..., visible = locs.visible )
+        end
+    end
+
+    if locs.height[] > 0
+        for (cs, ls) in zip(colors[][1:end], points[][1:end])
+            scatter!(locs, ls; color = cs, locs.above_attributes..., visible = locs.visible)
+        end
+    end
+
+
+    scatter!(locs, points_notselected.points; color = points_notselected.colors, locs.base_attributes..., visible = locs.visible)
+    scatter!(locs, point_selected.points; color = point_selected.colors, locs.base_attributes..., locs.selected_attributes..., visible = locs.visible)
+
+    locs
+end
+
+
+Makie.convert_arguments(P::Type{<:Locations}, selected::Union{Nothing,<:Integer}, locations::OffsetVector{<:AbstractVector{Location}}) = Makie.convert_arguments(P, Selected(selected), locations)
+Makie.convert_arguments(P::Type{<:Locations}, selected::Union{Nothing,<:Integer}, locations::Vector{<:AbstractVector{Location}}, layer::Integer) = Makie.convert_arguments(P, Selected(selected), OffsetVector(locations, -layer))
+Makie.convert_arguments(P::Type{<:Locations}, locations::Vector{<:AbstractVector{Location}}, layer::Integer) = Makie.convert_arguments(P, nothing, OffsetVector(locations, -layer))
+Makie.convert_arguments(P::Type{<:Locations}, locations::OffsetVector{<:AbstractVector{Location}}) = Makie.convert_arguments(P, nothing, locations)
