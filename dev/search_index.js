b941bc2509d6ede78529d1d866d62cb072c8e10d
Documenter.jl <documenter@juliadocs.github.io>
2022-04-18 13:38:40 +0000

    build based on ba8333e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ANT","category":"page"},{"location":"#ANT","page":"Home","title":"ANT","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ANT.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ANT]","category":"page"}]
+}
