4a9e599ab3570cba1d712fe330a7167c05d284a9
Martin Kunz <martinkunz@email.cz>
2022-05-11 10:48:14 +0200

    fix(dosc): change name

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,7 @@ DocMeta.setdocmeta!(ImageSequenceAnnotationRecipes, :DocTestSetup, :(using Image
 makedocs(;
     modules=[ImageSequenceAnnotationRecipes],
     authors="Martin Kunz <martinkunz@email.cz> and contributors",
-    repo="https://github.com/kunzaatko/ANT.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/kunzaatko/ImageSequenceAnnotationRecipes.jl/blob/{commit}{path}#{line}",
     sitename="ImageSequenceAnnotationRecipes.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
