2e52ee7520f87f74d96f15899d808e5a04b96918
Martin Kunz <martinkunz@email.cz>
2022-05-15 10:32:45 +0200

    feat(attributes, test): attributes to run on Attributes and GLMakie test

--- src/attributes.jl
+++ src/attributes.jl
@@ -1,55 +1,61 @@
 module AttributeModifiers
+using Makie: Attributes
 using ColorTypes
 # TODO: These should use Attributes instead <03-05-22> 
 # TODO: Make these more constructable, perhaps with a macro that makes the calls to the acctual
 # function with factor
-function saturation_key!(x::Dict, factor::Real, key::Symbol)
+function saturation_key!(x::Attributes, factor::Real, key::Symbol)
     @assert factor > 0 "The `factor` argument be between 0 and 1"
-    @assert key in [:color, :strokecolor]
-    col = HSLA(x[key])
+    @assert key in keys(x) "$key is not in attributes $x"
+    col = parse(Color, x[key][])
+    col = convert(HSLA, col)
     x[key] = HSLA(col.h, min(col.s * factor, 1.0), getfield.(col, [:l, :alpha])...)
 end
-dim!(x::Dict, factor = 0.2) = foreach(key -> saturation_key!(x, 1 - factor, key), [:color, :strokecolor])
-saturate!(x::Dict, factor = 0.2) = foreach(key -> saturation_key!(x, 1 + factor, key), [:color, :strokecolor])
-dim_color!(x::Dict, factor = 0.2) = saturation_key!(x, 1 - factor, :color)
-saturate_color!(x::Dict, factor = 0.2) = saturation_key!(x, 1 + factor, :color)
-dim_strokecolor!(x::Dict, factor = 0.2) = saturation_key!(x, 1 - factor, :strokecolor)
-saturate_strokecolor!(x::Dict, factor = 0.2) = saturation_key!(x, 1 + factor, :strokecolor)
+dim!(x::Attributes, factor = 0.2, keys = [:color, :strokecolor]) = foreach(key ->haskey(x, key) && saturation_key!(x, 1 - factor, key), keys)
+saturate!(x::Attributes, factor = 0.2, keys = [:color, :strokecolor]) = foreach(key -> haskey(x, key) && saturation_key!(x, 1 + factor, key), keys)
+dim_color!(x::Attributes, factor = 0.2) = saturation_key!(x, 1 - factor, :color)
+saturate_color!(x::Attributes, factor = 0.2) = saturation_key!(x, 1 + factor, :color)
+dim_strokecolor!(x::Attributes, factor = 0.2) = saturation_key!(x, 1 - factor, :strokecolor)
+saturate_strokecolor!(x::Attributes, factor = 0.2) = saturation_key!(x, 1 + factor, :strokecolor)
 
-function lightness_key!(x::Dict, factor::Real, key::Symbol)
+function lightness_key!(x::Attributes, factor::Real, key::Symbol)
     @assert factor > 0 "The `factor` argument be between 0 and 1"
-    @assert key in [:color, :strokecolor]
-    col = HSLA(x[key])
+    @assert key in keys(x) "$key is not in attributes $x"
+    col = parse(Color, x[key][])
+    col = convert(HSLA, col)
     x[key] = HSLA(getfield.(col, [:h, :s])..., min(col.l * factor, 1), col.alpha)
 end
-darken!(x::Dict, factor = 0.2) = foreach(key -> lightness_key!(x, 1 - factor, key), [:color, :strokecolor])
-lighten!(x::Dict, factor = 0.2) = foreach(key -> lightness_key!(x, 1 + factor, key), [:color, :strokecolor])
-darken_color!(x::Dict, factor = 0.2) = lightness_key!(x, 1 - factor, :color)
-lighten_color!(x::Dict, factor = 0.2) = lightness_key!(x, 1 + factor, :color)
-darken_strokecolor!(x::Dict, factor = 0.2) = lightness_key!(x, 1 - factor, :strokecolor)
-lighten_strokecolor!(x::Dict, factor = 0.2) = lightness_key!(x, 1 + factor, :strokecolor)
+darken!(x::Attributes, factor = 0.2, keys = [:color, :strokecolor]) = foreach(key -> haskey(x, key) && lightness_key!(x, 1 - factor, key), keys)
+lighten!(x::Attributes, factor = 0.2, keys = [:color, :strokecolor]) = foreach(key -> haskey(x, key) && lightness_key!(x, 1 + factor, key), keys)
+darken_color!(x::Attributes, factor = 0.2) = lightness_key!(x, 1 - factor, :color)
+lighten_color!(x::Attributes, factor = 0.2) = lightness_key!(x, 1 + factor, :color)
+darken_strokecolor!(x::Attributes, factor = 0.2) = lightness_key!(x, 1 - factor, :strokecolor)
+lighten_strokecolor!(x::Attributes, factor = 0.2) = lightness_key!(x, 1 + factor, :strokecolor)
 
-function transparency_key!(x::Dict, factor::Real, key::Symbol)
+function transparency_key!(x::Attributes, factor::Real, key::Symbol)
     @assert factor > 0 "The `factor` argument be between 0 and 1"
-    @assert key in [:color, :strokecolor]
-    x[key] = RGBA(x[key])
-    x[key] = RGBA(getfield.(x[key], [:r, :g, :b])..., min(x[key].alpha * factor, 1))
+    @assert key in keys(x) "$key is not in attributes $x"
+    col = parse(Color, x[key][])
+    col = convert(RGBA, col)
+    x[key] = RGBA(getfield.(col, [:r, :g, :b])..., min(col.alpha * factor, 1))
 end
-decrease_transparency!(x::Dict, factor = 0.2) = foreach(key -> transparency_key!(x, 1 + factor, key), [:color, :strokecolor])
-increase_transparency!(x::Dict, factor = 0.2) = foreach(key -> transparency_key!(x, 1 - factor, key), [:color, :strokecolor])
-decrease_transparency_color!(x::Dict, factor = 0.2) = transparency_key!(x, 1 + factor, :color)
-increase_transparency_color!(x::Dict, factor = 0.2) = transparency_key!(x, 1 - factor, :color)
-decrease_transparency_strokecolor!(x::Dict, factor = 0.2) = transparency_key!(x, 1 + factor, :strokecolor)
-increase_transparency_strokecolor!(x::Dict, factor = 0.2) = transparency_key!(x, 1 - factor, :strokecolor)
+decrease_transparency!(x::Attributes, factor = 0.2, keys = [:color, :strokecolor]) = foreach(key -> haskey(x, key) && transparency_key!(x, 1 + factor, key), keys)
+increase_transparency!(x::Attributes, factor = 0.2, keys = [:color, :strokecolor]) = foreach(key -> haskey(x, key) && transparency_key!(x, 1 - factor, key), keys)
+decrease_transparency_color!(x::Attributes, factor = 0.2) = transparency_key!(x, 1 + factor, :color)
+increase_transparency_color!(x::Attributes, factor = 0.2) = transparency_key!(x, 1 - factor, :color)
+decrease_transparency_strokecolor!(x::Attributes, factor = 0.2) = transparency_key!(x, 1 + factor, :strokecolor)
+increase_transparency_strokecolor!(x::Attributes, factor = 0.2) = transparency_key!(x, 1 - factor, :strokecolor)
 
-function marker!(x::Dict, marker)
+function marker!(x::Attributes, marker)
     # TODO: Should convert to unifiing marker type. Same as color. <02-05-22> 
+    @assert :marker in keys(x) "$key is not in attributes $x"
     x[:marker] = marker
 end
 
-function markersize!(x::Dict, factor::Real)
-    x[:markersize] = x[:markersize] * factor
+function markersize!(x::Attributes, factor::Real)
+    @assert :markersize in keys(x) "$key is not in attributes $x"
+    x[:markersize] = x[:markersize][] * factor
 end
-decrease_markersize!(x::Dict, factor = 0.2) = markersize!(x, 1 - factor)
-increase_markersize!(x::Dict, factor = 0.2) = markersize!(x, 1 + factor)
+decrease_markersize!(x::Attributes, factor = 0.2) = markersize!(x, 1 - factor)
+increase_markersize!(x::Attributes, factor = 0.2) = markersize!(x, 1 + factor)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,10 @@
 using ImageSequenceAnnotationRecipes
-# using ANT: Recipes.ImageSequenceInteractions as ImSeqInts
+using ImageSequenceAnnotationRecipes.Recipes
+using ImageSequenceAnnotationRecipes: AttributeModifiers as AM
+using ColorTypes
 using Test
 using Makie
+using GLMakie
 using GeometryBasics: Point
 
 @testset "ImageSequenceAnnotationRecipes.jl" begin
@@ -21,10 +24,147 @@ using GeometryBasics: Point
     end
 
 
-    # TODO: test in a different way  
-    # @testset "ImageSequenceInteractions" begin
-    #     x = 1 |> Observable
-    #     nf = ImSeqInts.NextFrame(Keyboard.k, (x))
+    @testset "Locations" begin
+        @testset "LocationsLayers" begin
+            locslen = 10
+            locscats = [:a, :b, :c, nothing]
+            locs = [Location(p, c) for (p, c) in zip(rand(Point2, locslen), rand(locscats, locslen))]
+            _, _, loclay = locationslayer(0, nothing, locs)
+            @test typeof(loclay) <: LocationsLayer
+        end
+    end
+
+    @testset "Attributes" begin
+        @testset "ColorModifiers" begin
+            ColorAttributeType = Union{<:Colorant,Symbol}
+
+            # NOTE: Test parsing and working of color modifiers in the ideal setup (all keys present) <kunzaatko> 
+            for f in [
+                AM.dim!,
+                AM.saturate!,
+                AM.dim_color!,
+                AM.dim_strokecolor!,
+                AM.saturate_color!,
+                AM.saturate_strokecolor!,
+                AM.darken!,
+                AM.lighten!,
+                AM.darken_color!,
+                AM.lighten_color!,
+                AM.darken_strokecolor!,
+                AM.lighten_strokecolor!,
+                AM.decrease_transparency!,
+                AM.increase_transparency!,
+                AM.decrease_transparency_color!,
+                AM.increase_transparency_color!,
+                AM.decrease_transparency_strokecolor!,
+                AM.increase_transparency_strokecolor!,]
+                at = Attributes(; color = :blue, strokecolor = :red)
+                f(at)
+                @test all(typeof.(to_value.(getproperty.(at, [:color, :strokecolor]))) .<: ColorAttributeType)
+            end
+
+            # NOTE: Testing whether the general color modifiers act even when a key is missing <kunzaatko> 
+            for f in [
+                AM.dim!,
+                AM.saturate!,
+                AM.darken!,
+                AM.lighten!,
+                AM.decrease_transparency!,
+                AM.increase_transparency!,
+            ]
+                at = Attributes(; color = RGBf(0.1, 0.1, 0.1))
+                f(at)
+                @test !haskey(at, :strokecolor)
+                @test typeof(to_value(at[:color])) <: ColorAttributeType
+            end
+
+            # NOTE: Testing that the specific color modifiers fail when key is missing <kunzaatko> 
+            for f in [
+                AM.dim_color!,
+                AM.dim_strokecolor!,
+                AM.saturate_color!,
+                AM.saturate_strokecolor!,
+                AM.darken_color!,
+                AM.lighten_color!,
+                AM.darken_strokecolor!,
+                AM.lighten_strokecolor!,
+                AM.decrease_transparency_color!,
+                AM.increase_transparency_color!,
+                AM.decrease_transparency_strokecolor!,
+                AM.increase_transparency_strokecolor!,
+            ]
+                at = Attributes(;)
+                @test all(haskey.(at, [:strokecolor, :color]) .== false)
+                @test_throws AssertionError f(at)
+            end
+
+            # NOTE: Testing whether modifiers that are opposite undo eachother <kunzaatko> 
+            for (keys, fs) in [
+                ([:color, :strokecolor], (AM.dim!,
+                    AM.saturate!)),
+                ([:color], (AM.dim_color!,
+                    AM.saturate_color!)),
+                ([:strokecolor], (AM.dim_strokecolor!,
+                    AM.saturate_strokecolor!)),
+            ]
+                col = RGBf(0.5, 0.5, 0.5)
+                at = Attributes(; color = col, strokecolor = col)
+                foreach(fs) do f
+                    f(at)
+                end
+                if length(keys) > 1
+                    @assert typeof(at[:color][]) == typeof(at[:strokecolor][])
+                end
+                col = convert(typeof(at[keys[1]][]), col)
+                @test all(isapprox.(to_value.(getproperty.(at, keys)), col, atol = 0.01))
+            end
+
+            # FIX: Works only for dim! and saturate! <kunzaatko> 
+            for (keys, fs) in [
+                ([:color, :strokecolor], (AM.darken!,
+                    AM.lighten!)),
+                ([:color, :strokecolor], (AM.decrease_transparency!,
+                    AM.increase_transparency!)),
+                ([:color], (AM.darken_color!,
+                    AM.lighten_color!)),
+                ([:strokecolor], (AM.darken_strokecolor!,
+                    AM.lighten_strokecolor!)),
+                ([:color], (AM.decrease_transparency_color!,
+                    AM.increase_transparency_color!)),
+                ([:strokecolor], (AM.decrease_transparency_strokecolor!,
+                    AM.increase_transparency_strokecolor!))
+            ]
+                col = RGBf(0.5, 0.5, 0.5)
+                at = Attributes(; color = col, strokecolor = col)
+                foreach(fs) do f
+                    f(at)
+                end
+                if length(keys) > 1
+                    @assert typeof(at[:color][]) == typeof(at[:strokecolor][])
+                end
+                col = convert(typeof(at[keys[1]][]), col)
+                @test_broken all(isapprox.(to_value.(getproperty.(at, keys)), col, atol = 0.01))
+            end
+
+
+        end
+
+        @testset "OtherModifiers" begin
+            at = Attributes(; marker = :circle, markersize = 4)
+            AM.marker!(at, :diamond)
+            @test at[:marker][] == :diamond
+            for f in (AM.increase_markersize!, AM.decrease_markersize!)
+                prevsize = at[:markersize][]
+                f(at)
+                @test prevsize != at[:markersize][]
+            end
+        end
+    end
+
+
+
+
+
     #     ImSeqInts.execute(nf)
     #     @test x[] == 2
     #     pf = ImSeqInts.PrevFrame(Keyboard.j, (x))
