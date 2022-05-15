4931af087021411d3573410961121799e1c1d674
Martin Kunz <martinkunz@email.cz>
2022-05-15 12:56:28 +0200

    edit/fix(locations): some fixes from tests and structvector
    This would be a bad API for the user to need to convert the input into a
    structvector... Use it as a lifted observable in the plot recipe for
    performance reasons but the user can supply a vector.
    
    Some tests detected problems with changind selected etc. these are fixed
    now.

--- src/recipes/locations/recipe.jl
+++ src/recipes/locations/recipe.jl
@@ -6,6 +6,7 @@ import Makie: convert_arguments
 using ColorTypes
 using DataStructures
 using ...AttributeModifiers
+using ColorSchemes
 export locationslayer, locationslayer!, LocationsLayer
 
 function offset_conversion(x::Attributes; offset = 0)
@@ -33,13 +34,10 @@ end
 
 @recipe(LocationsLayer) do scene
     Attributes(
+        fallback_attributes = Attributes(:color => ColorSchemes.julia[4], :markersize => 2.0, :marker => :circle, :rotations => 0.0, :strokecolor => :black),
         # FIX: There is no need for this dict if there are only symbol attributes. This should be
         # handled by the parent attributes.
-        # A dictionary that represents which attributes to use for the different categories. Must
-        # include `:fallback` key with a `NamedTuple` that includes all the keys that are used in
-        # the '..._conversion_fuction's
-        # NOTE: Using Dict, because NamedTuple type is immutable
-        # FIX: Use Attributes instead for the inner Dict <03-05-22> 
+        # A dictionary that represents which attributes to use for the different categories.
         annotation_attributes = Dict{Any,Attributes}(),
         # A function that will convert the attributes of the points based on the offset from the
         # reference layer. (Should include an `offset` keyword argument)
@@ -51,28 +49,24 @@ end
         max_categories = 8,
         visible = true
     )
-    # TODO: Add a Theme that determines the fallbackattributes  
 end
 
 argument_names(::Type{<:LocationsLayer}, numargs::Integer) = numargs == 3 && (:offset, :selected, :locations)
 
 function Makie.plot!(
-    loc_layer::LocationsLayer{<:Tuple{Integer,Selected,StructVector{Location}}})
+    loc_layer::LocationsLayer{<:Tuple{Integer,Selected,AbstractVector{Location}}})
 
-    # TODO: From Theme 
-    fallback_attrs = Attributes(:color => RGBA(0.847, 0.871, 0.914, 1.0), :markersize => 2.0, :marker => :circle, :rotations => 0.0, :strokecolor => RGBA(0.231, 0.259, 0.322, 1.0))
-
-
+    locations_structvector = @lift StructVector($(loc_layer[:locations]))
     # Cached selected for when the restoring the previous selected to the normal state on change
     # NOTE: Changes on change of `loc_layer[:selected]` <kunzaatko> 
     selectedcache = loc_layer[:selected][]
-    catbitarrays = lift(loc_layer[:locations]) do locs
+    catbitarrays = lift(locations_structvector) do locs
         d = Dict(
             (cat !== nothing ? cat : :fallback) => (locs.category .== cat)
             for cat in unique(locs.category)
         )
         if !isnothing(selectedcache)
-            selected_category = loc_layer[:locations][].category[selectedcache.idx]
+            selected_category = locs.category[selectedcache.idx]
             d[selected_category][selectedcache.idx] = 0
             d[:selected] = zeros(Bool, length(locs))
             d[:selected][selectedcache.idx] = true
@@ -90,32 +84,32 @@ function Makie.plot!(
     # FIX: This is what merge! should do (#1939) <kunzaatko> 
     function set_attributes(attrs::Attributes, new::Attributes)
         for (k, v) in new
-        @debug "Setting $k from $(attrs[k][]) to $(v[])"
             attrs[k] = v[]
         end
     end
 
     # FIX: `merge` insonsistent. When #1939 resolved, can be fixed <kunzaatko> 
     get_base_attributes(category) = haskey(loc_layer.annotation_attributes[], category) ?
-                                    merge(loc_layer.annotation_attributes[][category], fallback_attrs, loc_layer.scatter_attributes) : merge(fallback_attrs, loc_layer.scatter_attributes)
+                                    merge(loc_layer.annotation_attributes[][category], loc_layer.fallback_attributes, loc_layer.scatter_attributes) : merge(loc_layer.fallback_attributes, loc_layer.scatter_attributes)
 
     loc_layer_cats = Dict{Any,NamedTuple{(:scatter, :positions, :base_attrs)}}()
 
     # NOTE: It is not possible to define more plots for the recipe after it finishes running. We
     # have to predefine some. <kunzaatko> 
     scatters = Vector(undef, loc_layer.max_categories[])
-    @async for i in Base.OneTo(loc_layer.max_categories[])
+    scatters_task = @async for i in Base.OneTo(loc_layer.max_categories[])
         positions = Point2[] |> Observable
-        scatter = scatter!(loc_layer, positions; fallback_attrs..., visible = loc_layer.visible)
+        scatter = scatter!(loc_layer, positions; loc_layer.fallback_attributes..., visible = loc_layer.visible)
         scatters[i] = (; scatter = scatter, positions = positions)
     end
 
     on(catbitarrays) do cbitarrays
         for (cat, idxs) in cbitarrays
             if !haskey(loc_layer_cats, cat) # create new entry
+                wait(scatters_task)
                 scatter, positions = pop!(scatters)
                 base_attrs = get_base_attributes(cat)
-                positions[] = loc_layer[:locations][][idxs].point
+                positions[] = locations_structvector[].point[idxs]
                 @debug "Creating new scatter for category $cat with base attributes $base_attrs"
                 loc_layer_cats[cat] = (; scatter = scatter, positions = positions, base_attrs = base_attrs)
 
@@ -126,7 +120,7 @@ function Makie.plot!(
 
             else # update the points
                 @debug "Updating category for category $cat"
-                loc_layer_cats[cat][:positions][] = loc_layer[:locations][][idxs].point
+                loc_layer_cats[cat][:positions][] = locations_structvector[].point[idxs]
             end
         end
     end
@@ -143,16 +137,20 @@ function Makie.plot!(
     on(loc_layer[:selected]) do selected
         empty!(loc_layer_cats[:selected][:positions][])
         if !isnothing(selectedcache) # restore prev selected
-            c, p = loc_layer[:locations][].category[selectedcache.idx], loc_layer[:locations][].point[selectedcache.idx]
+            @debug "Updating locations on selectedcache $selectedcache"
+            c, p = locations_structvector[].category[selectedcache.idx], locations_structvector[].point[selectedcache.idx]
+            c = c === nothing ? :fallback : c
             catbitarrays.val[c][selectedcache.idx] = 1 # set without notify
             push!(loc_layer_cats[c][:positions][], p)
             notify(loc_layer_cats[c][:positions])
         end
         if !isnothing(selected)
-            c, p = loc_layer[:locations][].category[selected.idx], loc_layer[:locations][].point[selected.idx]
+            @debug "Updating locations on selected $selected"
+            c, p = locations_structvector[].category[selected.idx], locations_structvector[].point[selected.idx]
+            c = c === nothing ? :fallback : c
             catbitarrays.val[c][selected.idx] = 0 # set without notify
             push!(loc_layer_cats[:selected][:positions][], p)
-            loc_layer_cats[c][:positions][] = loc_layer[:locations][][catbitarrays[][c]].point
+            loc_layer_cats[c][:positions][] = locations_structvector[].point[catbitarrays[][c]]
         end
         notify(loc_layer_cats[:selected][:positions])
         selectedcache = selected
@@ -160,7 +158,7 @@ function Makie.plot!(
 
     # Initial scatters (this is for making the additional scatter initializations @async)
     for (cat, idxs) in catbitarrays[]
-        positions = loc_layer[:locations][][idxs].point |> Observable
+        positions = locations_structvector[].point[idxs] |> Observable
         scatter = scatter!(loc_layer, positions; visible = loc_layer.visible)
         base_attrs = get_base_attributes(cat)
         loc_layer_cats[cat] = (; scatter = scatter, positions = positions, base_attrs = base_attrs)
@@ -171,4 +169,4 @@ function Makie.plot!(
     return loc_layer
 end
 
-convert_arguments(P::Type{<:LocationsLayer}, offset::Integer, selected::Union{Nothing,Integer}, locations::AbstractVector{Location}) = convert_arguments(P, offset, Selected(selected), StructVector(locations))
+convert_arguments(P::Type{<:LocationsLayer}, offset::Integer, selected::Union{Nothing,Integer}, locations::AbstractVector{Location}) = convert_arguments(P, offset, Selected(selected), locations)
